e73281c07fd225b798c14683e971660569a7d87c
Chris Whiteley <chris.whiteley81@gmail.com>
2024-12-19 16:35:26 +0000

    Encapsulation in Startup:
    The handlers and beanSupplier fields are now private and final, ensuring immutability.

--- src/main/java/com/cwsoft/eventrouter/Startup.java
+++ src/main/java/com/cwsoft/eventrouter/Startup.java
@@ -9,21 +9,35 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public abstract class Startup {
 
-    public EventHandlers handlers;
-    public BeanSupplier beanSupplier;
+    private final EventHandlers handlers;
+    private final BeanSupplier beanSupplier;
 
-    /**
-     * this should be called once the application context (BeanSupplier) has been initialised with all beans in the
-     * service (application).  It sets up all the handlers based on beans that have methods annotated with @EventHandler.
-     * Once initialisation is complete it dispatches the event "RebasoftContextReady" as certain beans may want to be made aware of this.
-     */
+    public Startup(EventHandlers handlers, BeanSupplier beanSupplier) {
+        if (handlers == null || beanSupplier == null) {
+            throw new IllegalArgumentException("handlers and beanSupplier must not be null");
+        }
+        this.handlers = handlers;
+        this.beanSupplier = beanSupplier;
+    }
+
+   /*
+    * this should be called once the application context (BeanSupplier) has been initialised with all beans in the
+    * service (application).  It sets up all the handlers based on beans that have methods annotated with @EventHandler.
+    * Once initialisation is complete it dispatches the event todo: change name "RebasoftContextReady" as certain beans may want to be made aware of this.
+    */
     public void start() {
-        if (handlers == null) throw new NullPointerException("Required field handlers has not been set");
-        EventDispatcher  eventDispatcher = EventDispatcher.instanceOf;
-        if (eventDispatcher == null) throw new IllegalStateException("Event Dispatcher has not been created");
-        if (beanSupplier == null) throw new NullPointerException("Required field beanSupplier has not been set");
+        log.info("Starting EventRouter initialization...");
+        EventDispatcher eventDispatcher = EventDispatcher.instanceOf;
+
+        if (eventDispatcher == null) {
+            throw new IllegalStateException("EventDispatcher has not been created");
+        }
+
         handlers.init(beanSupplier);
-        // rebasoft-context (RebasoftApplicationContext and Event Dispatcher) now ready!
-        eventDispatcher.dispatchEvent(new NamedEvent("RebasoftContextReady"));
+        log.info("EventRouter initialized successfully.");
+
+        NamedEvent contextReadyEvent = new NamedEvent("RebasoftContextReady");
+        eventDispatcher.dispatchEvent(contextReadyEvent);
+        log.info("Dispatched event: {}", contextReadyEvent.getName());
     }
 }
